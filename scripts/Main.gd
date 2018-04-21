7d43d87631aab00e5875107b8958701721bb91fd
phrb <pedro.bruel@gmail.com>
2018-04-21 14:37:12 +0200

    Add Table, Card and Main scripts

---
+++ scripts/Main.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+const TABLE_PATH = "Table.gd"
+const TABLE_CLASS = preload(TABLE_PATH)
+
+const CARD_PATH = "Card.gd"
+const CARD_CLASS = preload(CARD_PATH)
+
+func _ready():
+	var table = TABLE_CLASS.new(10, "player_1", "player_2")
+	var card = CARD_CLASS.new(0, "test_name")
+	
+	table.print_table()
+	
+	table.add_card("player_1", card)
+	table.print_table()
+	
+	table.add_card("player_2", card)
+	table.print_table()
+	
+	table.remove_card("player_1", card)
+	table.print_table()
+	
+	table.clear_side("player_2")
+	table.print_table()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
